0f00aad16a33c564747e46f1125cdd63791bdd75
Sovelesss <rickkrtz@Gmail.com>
2022-12-04 22:24:40 -0500

    Finished user entry selection, and created a list that breaks down and condenses into a usable list comprising of words, numbers, '', \n, parens, brackets, etc...

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
-
 FoI = ''
 FName = ''
 sCode = []
 aCode = ''
+alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1'
+    ,'2','3','4','5','6','7','8','9','0']
 #The following segment will allow the program to read from a file, or read from user input
 while True:
     FoI = input("Do you wish to enter a filename? [Y/N]\n")
@@ -15,30 +16,55 @@ while True:
             for line in userFile:
                 #Write to workingFile
                 workingFile.write(line)
-
-        #code here should move the text found in the file to the variable sCode
-
         break
     elif FoI.upper() == "N":
         print("Enter you code, you may enter multiple lines, finish by entering an empty line")
         while True:
             user_input = input()
-
             #if user presses Enter without a value, break out of loop
             if user_input == '':
                 break
             else:
                 sCode.append(user_input)
-
         with open(r'Working.txt', 'w') as fp:
             for item in sCode:
                 # write each item on a new line
                 fp.write("%s\n" % item)
-
         break
     else:
         continue
 
-content = open(r'Working.txt', 'r').read()
-print("file content:\n" + content)
-#Actual parsing begins
+content = open(r'Working.txt', 'r').read() #TBD
+print("file content:\n" + content) #TBD
+
+#breaks the file into a list, containing individual characters
+charList = []
+file = open('Working.txt','r')
+while True:
+    char = file.read(1)
+    if not char:
+        break
+    charList.append(char)
+file.close()
+print(charList) #TBD
+
+#condenses the list into better, more usable parts
+condList = []
+temp = ''
+for x in charList:
+
+    if x.upper() in alphabet:
+        temp = temp + x
+    else:
+        if condList != '':
+            condList.append(temp)
+        condList.append(x)
+        temp = ''
+while True:
+    if '' in condList:
+        condList.remove('')
+    else:
+        break
+print(condList) #TBD
+
+
