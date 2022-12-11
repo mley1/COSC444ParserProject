8e9d98fdca7a446070b48896196f44e94c7ae103
mley1 <47510116+mley1@users.noreply.github.com>
2022-12-11 16:26:46 -0500

    Updated checkFor

--- checkFor.py
+++ checkFor.py
@@ -83,15 +83,23 @@ def checkFor(inputList):
         if inputList[i] != ' ':
             shortList[j] = inputList[i]
             j = j+1
-            break;
+            break
         break
 
     for i in range(len(shortList)):
-         if i/2 ==0:
-             if shortList [i] == 'for' or shortList [i] == ':=' or shortList [i] == 'to' or shortList [i] == 'do' or shortList [i] == ':'
-                 currentValue = 1
+        if i == 0 and shortList[i] == 'for':
+            currentValue = 1
         else:
             currentValue = 0
+        if i == 4 and shortList[i] == 'to':
+            currentValue = 1
+        else:
+            currentValue = 0
+        if i == 6 and shortList[i] == 'do':
+            currentValue = 1
+        else:
+            currentValue = 0
+
     if currentValue == 1:
         return True
     else:
