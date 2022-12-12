655b032ade8067680449b68e439f37ad5a79302a
Sovelesss <rickkrtz@Gmail.com>
2022-12-11 20:42:12 -0500

    3 branches merged, and cleaned up.

--- variable.py
+++ variable.py
@@ -1,6 +1,6 @@
-
 def isType(typeVal):
-    typeList = ["Integer", "Real", "Character", "Boolean", "String"]
+    typeVal = typeVal.upper()
+    typeList = ["INTEGER", "REAL", "CHARACTER", "BOOLEAN", "STRING"]
     check = False
     for i in range(5):
         if typeVal == typeList[i]:
@@ -10,27 +10,38 @@ def isType(typeVal):
 
 def isName(nameVal):
     check = False
-    nameVal.upper()
+    nameVal = nameVal.upper()
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
                 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    for x in nameVal:
+    for x in range(len(nameVal)):
         if not nameVal[x] in alphabet:
             check = False
+        else:
+            check = True
     return check
 
 
 def CheckVar(varList):
     multiVar = False
     check = True
+    spaceList = []
     colonCheck = 0
     semiCheck = 0
     equalCheck = 0
     commaCheck = 0
 
-    if varList[len(varList)-2] != ';':
+    for i in range(len(varList)):
+        if varList[i] == ' ':
+            spaceList.append(i)
+    for i in range(len(spaceList)):
+        if varList[spaceList[i] - i] == ' ':
+            varList.pop(spaceList[i] - i)
+
+    if varList[len(varList) - 2] != ';':
+        print(';')
         check = False
         return check
-    for i in range(len(varList)):
+    for i in range(len(varList) - 1):
         if varList[i] == ':':  # Checks for more than 1 colon
             colonCheck += 1
             if colonCheck > 1:
@@ -46,31 +57,60 @@ def CheckVar(varList):
             if equalCheck > 1:
                 check = False
                 return check
-        elif varList[i] == ' ':
-            varList.pop(i)
-        else:
-            if varList[i] == ',':  # Checks for more than 1 comma
-                commaCheck += 1
-                if commaCheck > 1:
-                    multiVar = True
+        elif varList[i] == ',':  # Checks for more than 1 comma
+            commaCheck += 1
+            if commaCheck >= 1:
+                multiVar = True
     if not multiVar:
-        if varList[1] != ':':
-            check = False
-        if varList[3] != '=':
-            check = False
+        if len(varList) > 5:
+            if not isName(varList[0]):
+                check = False
+            if varList[1] != ':':
+                check = False
+            if not isType(varList[2]):
+                check = False
+            if not varList[3] == '=':
+                check = False
+            if not varList[5] == ';':
+                check = False
+        else:
+            if not isName(varList[0]):
+                check = False
+            if varList[1] != ':':
+                check = False
+            if not isType(varList[2]):
+                check = False
+            if varList[3] != ';':
+                check = False
     if multiVar:
-        for i in range(0, len(varList) - 3, 2):
+        for i in range(0, len(varList) - 5, 2):
             if not isName(varList[i]):
                 check = False
-        for i in range(1, len(varList) - 4, 2):
+        if not isName(varList[len(varList) - 5]):
+            check = False
+        for i in range(1, len(varList) - 6, 2):
             if not varList[i] == ',':
                 check = False
-        if not isType(varList[len(varList) - 2]):
+        if not varList[len(varList) - 4] == ':':
+            check = False
+        if not isType(varList[len(varList) - 3]):
+            check = False
+        if not varList[len(varList) - 2] == ';':
             check = False
     return check
 
 
-# varname : type ;
-# varname : type = value;
+# not fully implemented
+def CheckVarAssignment(varStmt):
+    check = True
+    if not isName(varStmt[0]):
+        check = False
+    if not varStmt[1] == ':':
+        check = False
+    # check if value is valid here
+    if not varStmt[3] == ';':
+        check = False
+    return check
 
-# varname := value; post declaration assignment/reassignment outside scope of this function
+
+# name = value ;
